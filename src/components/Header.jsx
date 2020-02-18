aede248d3ae092d006c60c94827b86b2bb0f9da1
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-18 18:13:10 +0200

    Completed by header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+//import photo
+import PhotoProfile from '../img/photo-profile.jpg';
 function Header() {
     return (
       <header>
-          
+          <div className="container">
+            <div className="header">  
+              <img src={PhotoProfile} alt="profile" className="header-photo"/>
+            </div>
+          </div>
       </header>
     );
   }
