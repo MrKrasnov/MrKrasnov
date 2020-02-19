02fc8817a18fe1f3239f0a357dcc4a87fd766e56
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-19 15:15:53 +0200

    Performed the markup other.jsx

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 function Other(){
     return(
         <div>
-            OTHER
+            <div className="container">
+            <h1 className="other">Other</h1>
+            <div className="other-flex">
+                <div className="other-flex-skill"> 
+                    <ul><h2>Skill:</h2>
+                        <li>HTML, CSS3</li>
+                        <li>Gulp, SASS</li>
+                        <li className="other-flex-down">
+                        JavaScript - ES5+
+                        </li>
+                        <li>Vue.js -basics, work with components.</li>
+                        <li className="other-flex-down">
+                        React.js - Basics, work with JSX and properties.
+                        </li>
+                        <li>
+                        Node.js - general understanding.
+                        </li>
+                        <li>
+                        PHP - work with the database, used orm redbean.
+                        </li>
+                    </ul>
+                </div>
+                <div className="other-flex-skill-education">
+                    <h2>Specialized education:</h2>
+                    <p>Odessa College of Computer Technology, 2015–2019, <br/>
+                    <span>specialty:</span> technician constructor (junior specialist)
+                    </p>
+                    <div>
+                        <h3>This site was developed by React.js</h3>
+                        <a href="https://github.com/MrKrasnov/MrKrasnov">GitHub</a>    
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
