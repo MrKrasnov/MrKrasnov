11cdc17f5df5b270abdea8f85d602e52bf67db3f
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-19 14:22:11 +0200

    Added buttons and description

--- src/components/Portfolio/Description.jsx
+++ src/components/Portfolio/Description.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Description(){
     return(
         <div>
-
+            <p>project title</p>
+            <div className="portfolio-title-grid-link">
+                <a href="/">Git Hub</a>
+                <a href="/">Link</a>
+            </div>
         </div>
     )
 }
