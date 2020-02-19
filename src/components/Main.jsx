d3c894cc5b3639484f1926580536f9f7ac263664
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-19 11:37:52 +0200

    Added basic information about myself

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 
 function Main(){
     return(
-        <div className="main">
+        <div>
             <div className="container">
-                <div className="main-content">
-                    <h1>general information</h1>
-                    <ul>
-                        <li><a href="">Telegram</a></li>
-                        <li><a href="">LinkedIn</a></li>
-                        <li><a href="">Summary</a></li>
-                    </ul>
+                <div className="main">
+                    <h1 className="main-title">General information</h1>
+                    <div className="main-title-flex">
+                        
+                        <ul className="main-title-navigation">
+                            <li><a href="https://t.me/mrKrasnov">Telegram</a></li>
+                            <li><a href="https://www.linkedin.com/in/yuri-krasnov-9468b019b/">LinkedIn</a></li>
+                            <li><a href="https://drive.google.com/file/d/1_SJxqcxWNbeM3iom4I4bVTWsHkcQakEG/view">Summary</a></li>
+                        </ul>
+                        <div className="main-title-flex-distribution">
+                            <p className="main-title-flex-distribution-speciality">Front-end Developer</p>
+                            <p>Date of Birth: June 02, 1999</p>
+                            <p>Work in freelance 7 months. I was engaged in the support of <br/>
+                               sites promoting cryptocurrency.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
