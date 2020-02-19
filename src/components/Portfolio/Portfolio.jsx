bada47b6b11ae14a5f098e0b9aeb2cd1552de2f6
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-19 14:09:37 +0200

    Connect Description.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import WebStudio from '../../img/portfolio/WebStudio.JPG';
 import Vue from '../../img/portfolio/vue.jpg';
 import APPTANK from '../../img/portfolio/APPTANK.JPG';
 import Game from '../../img/portfolio/Game.JPG';
+import Description from './Description';
 function Portfolio(){
     return(
         <div className="portfolio">
@@ -11,11 +12,17 @@ function Portfolio(){
                 <h1 className="portfolio-title">Portfolio</h1>
                 <div className="portfolio-title-grid">
                     <div><img src={WebStudio} alt=""/>
-                        
+                        <Description/>
                     </div>
-                    <div><img src={Vue} alt=""/></div>
-                    <div><img src={APPTANK} alt=""/></div>
-                    <div><img src={Game} alt=""/></div>
+                    <div><img src={Vue} alt=""/>
+                        <Description/>
+                    </div>
+                    <div><img src={APPTANK} alt=""/>
+                        <Description/>    
+                    </div>
+                    <div><img src={Game} alt=""/>
+                        <Description/>
+                    </div>
                 </div>
             </div>
         </div>
