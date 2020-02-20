7eaa70094c5d302c648ca3ef72ba54662d427a24
MrKrasnov <mr.krasnov.y@gmail.com>
2020-02-21 01:12:36 +0200

    Added experimental component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Other from './components/Other';
 import Portfolio from './components/Portfolio/Portfolio';
+import Test from './components/Test';
 
 function App() {
   return (
     <div>
       <Header/>
       <Main/>
+      <Test/>
       <Portfolio/>
       <Other/>
     </div>
